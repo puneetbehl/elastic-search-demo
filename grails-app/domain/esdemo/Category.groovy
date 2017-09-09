a1467d9bd6964d3fde403f2825b0202a61d2516a
Richa <richa@tothenew.com>
2017-09-09 15:54:54 +0530

    Domain To Test Calendar/Date as transient fields

---
+++ grails-app/domain/esdemo/Category.groovy
@@ -0,0 +1,14 @@
+package esdemo
+
+class Category {
+
+    Date createdDate
+    Calendar lastUpdated
+    Set<String> names
+
+    static transients = ['lastUpdated' , 'names']
+    static searchable = {
+
+        only = ['lastUpdated', 'createdDate' , 'names']
+    }
+}
